b6abc8d8b229569cba2b1aa5aa1e4644deafbbd6
Mariah <48637409+mdiazXV@users.noreply.github.com>
2020-05-10 17:03:51 -0400

    changed scope of variables and added detection for when certain keys are pressed

--- Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -4,21 +4,46 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        int max = 1000;
-        int min = 1;
 
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number, but don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //The following cocde to detect if up arrow was pressed was found at https://docs.unity3d.com/ScriptReference/KeyCode.UpArrow.html
+        //Detect when the up arrow key is pressed down
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+            min = guess;
+            Debug.Log(guess);
+        }
+
+        //Detect when down key is pressed
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed.");
+            max = guess;
+            Debug.Log(guess);
+        }
+
+        //Detect when enter/return key is pressed
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You hit enter.");
+        }
     }
 }
