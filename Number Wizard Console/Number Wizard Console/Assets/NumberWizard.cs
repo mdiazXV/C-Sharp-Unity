83baa1078d4af8061ebb7a29d7e33aec097ba32e
Mariah <48637409+mdiazXV@users.noreply.github.com>
2020-05-10 16:22:44 -0400

    Added variables for min and max numbers, altering user's instructions

--- Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -7,7 +7,13 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard!");
+        Debug.Log("Pick a number, but don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
     }
 
     // Update is called once per frame
