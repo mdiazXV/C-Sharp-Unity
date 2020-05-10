52b9a40181d8d0cc8b8ff158167f62c65d758f80
Mariah <48637409+mdiazXV@users.noreply.github.com>
2020-05-10 18:59:03 -0400

    added an update to guess variable

--- Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -29,6 +29,7 @@ public class NumberWizard : MonoBehaviour
         {
             Debug.Log("Up Arrow key was pressed.");
             min = guess;
+            guess = (max + min) / 2;
             Debug.Log(guess);
         }
 
@@ -37,6 +38,7 @@ public class NumberWizard : MonoBehaviour
         {
             Debug.Log("Down Arrow key was pressed.");
             max = guess;
+            guess = (max + min) / 2;
             Debug.Log(guess);
         }
 
